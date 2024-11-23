454f12912a21854655ffcd9bbb918bd80b0a8450
keysha2626 <kesyalee88@gmail.com>
2024-11-23 18:58:37 +0700

    push

---
+++ belajar_js/variable.js
@@ -0,0 +1,64 @@
+//javascript variables
+// 1. menggunakan var 
+//2. menggunakan const 
+//3. menggunakan let
+
+// ini adalah var 
+
+var keysha = 16
+var wahyu = 23
+var umur = keysha + wahyu 
+console.log(umur)
+
+
+// ini adalah let
+
+//ini adalah variable let
+let jumlahMurid = 300
+let jumlahGuru = 20
+let totalSemua = jumlahGuru + jumlahMurid
+console.log(totalSemua)
+
+
+
+//ini adalah variable cons
+const gajiProgramer = 10000000
+const gajiCeo = 30000000
+const totalGajiPtnusatrack = gajiProgramer + gajiCeo
+console.log(totalGajiPtnusatrack)
+
+
+//apa aja operator
+//  +, -, * ,** ,/, % , ++ , -- 
+
+
+// tipe data 
+// string , number, bigint, boolean , undefined, null, simbol, object
+
+//string
+let namaDepan = "kesya"
+let namaBelakang = "dapinasari"
+let namaKu =  namaDepan + namaBelakang
+console.log(namaKu)
+
+
+
+
+//tipe data boolean
+let jumlahSiswa = 30
+let JumlahGuru = 5
+let berapaJumlahsemua = jumlahSiswa >= jumlahGuru
+console.log(berapaJumlahsemua);
+
+//tipe data objek
+const biodata = {nama:"keysha",warnakulit:"coklat",umur:16}
+console.log(biodata.umur);
+
+//tipe data Array
+const datamurid = ["ucok","udin", "sitompul","ayam","hantu","kucing","harimau","singa"]
+console.log(datamurid[3]);
+
+
+let hewan;
+
+let usia=""
